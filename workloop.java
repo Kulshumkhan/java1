765105db309ccd061df6d815699ac904409c4d15
zakiya khan <khatoonzakiya03@gmail.com>
2025-01-10 10:04:42 +0530

    Add files via upload

---
+++ workloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class workloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        //boolean rem = n%2 ==0;
+        for (int i = 0; i < n; i++)
+        {if (i%2==0)
+            System.out.println(i);
+        }
+
+    }
+    }
