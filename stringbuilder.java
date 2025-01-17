c60ae1020fb141d4f8b91b4c5b72897a771a642d
zakiya khan <khatoonzakiya03@gmail.com>
2025-01-18 05:20:20 +0530

    Add files via upload

---
+++ stringbuilder.java
@@ -0,0 +1,27 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("zakiya");
+        System.out.println(sb);
+        //index charat
+       /* for (int i=0;i<sb.length();i++){
+            System.out.println(sb.charAt(i));*/
+        //index setcharat
+      //  sb.setCharAt(0,'s');
+      //  System.out.println(sb);
+        //intex insert
+       // sb.insert(0,'s');
+       // System.out.println(sb);
+        //index dlt
+        //sb.delete(0,2);
+        //System.out.println(sb);
+        // index append to join alphabed
+        sb.append("h");//str=str+"z
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+
+        }
+    }
+
---
+++ strings.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sentence="my name is zakiya.";
+       // String name =sentence.substring(0,sentence.length());
+        String name =sentence.substring(0,7);
+        System.out.println(name);
+      /*  String firstname="zakiya";
+        String lastname="khatoon";
+      String fullname=lastname+firstname;
+        System.out.println(fullname);*/
+       // String firstname=sc.nextLine();
+      //  String lastname=sc.nextLine();
+       // String fullname =firstname+" "+lastname;
+       // System.out.println(fullname.length());
+       // for (int i=0;i<fullname.length();i++){
+         //   System.out.println(fullname.charAt(i));
+           // if (firstname.compareTo(lastname)==0){
+           //     System.out.println("both are equall");
+            //}
+            //else {
+              //  System.out.println("not equall");
+            //}
+        //strings are immutable.
+        }
+    }
+
